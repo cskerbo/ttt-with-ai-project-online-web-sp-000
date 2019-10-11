ff59ba881d8b138413ddb69875033d22c4eab134
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-11 20:59:54 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -26,7 +26,8 @@ class Game
   def won?
     WIN_COMBINATIONS.detect do |combo|
       @board.cells[combo[0]] == @board.cells[combo[1]] &&
-      @board.cells[combo[1]] == @board.cells[combo[2]]
+      @board.cells[combo[1]] == @board.cells[combo[2]] &&
+      @board.taken?(combo[0])
     end
   end
 
