08d24c69f905004e4652ba4a1987e0d18771b962
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-11 20:51:13 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -23,4 +23,12 @@ class Game
     @board.turn_count % 2 == 0 ? player_1 : player_2
   end
 
+  def won?
+    WIN_COMBINATIONS.detect do |combo|
+      @board[combo[0]] == @board[combo[1]] &&
+      @board[combo[1]] == @board[combo[2]] &&
+      position_taken?(combo[0])
+    end
+  end
+
 end
